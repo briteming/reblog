36b73674c3be2634c9bfcb0d04f9196cb3079128
briteming <briteming@gmail.com>
2024-12-17 17:27:17 -0800

    Update config.js

--- src/constants/config.js
+++ src/constants/config.js
@@ -6,6 +6,5 @@ export const REPONAME = 'reblog';
 
 export const CLIENT_ID = 'Ov23liq0VjxWPMAVb8Ig';
 export const CLIENT_SECRET = 'd9ca3c22bf57be1920a5cf47c4baa7c960981d36';
-export const SITE_URL = 'https://github.com/briteming/reblog';
 
 export const POSTS_PER_PAGE = 3;
