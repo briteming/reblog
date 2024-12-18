9a2b5b75b48d5a7a058de753a4931d8e20a0c591
briteming <briteming@gmail.com>
2024-12-17 22:07:27 -0800

    Update config.js

--- src/constants/config.js
+++ src/constants/config.js
@@ -7,4 +7,6 @@ export const REPONAME = 'reblog';
 export const CLIENT_ID = 'Ov23lipR5M5LXgCCKU21';
 export const CLIENT_SECRET = 'a367601aa2a356b9cdc61da01c723835262b3a29';
 
+export const SITE_URL = 'http://rb.briten.top';
+
 export const POSTS_PER_PAGE = 3;
