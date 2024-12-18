75a09c39ac06127273fece400167551cb5240150
briteming <briteming@gmail.com>
2024-12-17 17:24:04 -0800

    Create config.js

---
+++ src/constants/config.js
@@ -0,0 +1,11 @@
+export const BLOG_NAME = 'Reblog of brite';
+export const BLOG_SLOGAN = 'welcome to my blog';
+
+export const USERNAME = 'briteming';
+export const REPONAME = 'reblog';
+
+export const CLIENT_ID = 'Ov23liq0VjxWPMAVb8Ig';
+export const CLIENT_SECRET = 'd9ca3c22bf57be1920a5cf47c4baa7c960981d36';
+export const SITE_URL = 'https://github.com/briteming/reblog';
+
+export const POSTS_PER_PAGE = 3;
