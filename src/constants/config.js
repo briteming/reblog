24e1fbb09da7c79c04caf5987845ae6baadc54db
briteming <briteming@gmail.com>
2024-12-17 22:02:50 -0800

    Update config.js

--- src/constants/config.js
+++ src/constants/config.js
@@ -4,7 +4,7 @@ export const BLOG_SLOGAN = 'welcome to my blog';
 export const USERNAME = 'briteming';
 export const REPONAME = 'reblog';
 
-export const CLIENT_ID = 'Ov23liq0VjxWPMAVb8Ig';
-export const CLIENT_SECRET = 'd9ca3c22bf57be1920a5cf47c4baa7c960981d36';
+export const CLIENT_ID = 'Ov23lipR5M5LXgCCKU21';
+export const CLIENT_SECRET = 'a367601aa2a356b9cdc61da01c723835262b3a29';
 
 export const POSTS_PER_PAGE = 3;
